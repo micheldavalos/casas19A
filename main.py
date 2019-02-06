48a70609f707ea13d637ebe2a5f398701e90e081
Michel Dávalos <michel.davalos@academicos.udg.mx>
2019-02-06 14:27:35 -0600

    se obtienen las urls siguientes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-url = 'http://aviso.informador.com.mx/index.php/bienes-raices'
+url = 'http://aviso.informador.com.mx/index.php/bienes_raices/busqueda?selecciono=1&ciudad_autocomplete=0&colonia_autocomplete=&transaccion=1&tipo=1&consulta=Zona+Metropolitana&precio_min=min&precio_max=max&recamaras_min=0&recamaras_max=0&metros_min=0&metros_max=0&quick-search=Zona+metropolitana-&quick-searchZap=Zapopan-3&quick-searchGdl=Guadalajara-2&quick-searchTlaq=Tlaquepaque-5&quick-searchTon=Tonal%C3%A1-4'
 r = requests.get(url)
 r.encoding = 'utf-8'
 # print(r.text)
@@ -31,6 +31,16 @@ for c in casas:
 with open('informador.json', 'w') as archivo:
     json.dump(lista, archivo, sort_keys=False, indent=4)
 
+paginas = soup.find(class_="pagination")
+paginas = paginas.find_all('li')
+urls = []
+i = 2
+while i < len(paginas) - 1:
+    # print(paginas[i].a['href'])
+    urls.append(paginas[i].a['href'])
+    i = i +1
+
+# print(urls)
 # print(lista)
 # c = casas[0]
 # ubicacion = c.find_all(class_='location')[0].text
